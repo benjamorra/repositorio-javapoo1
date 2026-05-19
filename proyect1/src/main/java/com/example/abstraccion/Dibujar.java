768a56fd524d9c784d08d11b2e908086d1874d9f
benjamorra <benjamorra24@gmail.com>
2026-05-19 10:14:17 -0300

    dev b segundo commit

--- proyect1/src/main/java/com/example/abstraccion/Dibujar.java
+++
@@ -1,6 +0,0 @@
-package com.example.abstraccion;
-
-public interface Dibujar {
-
-    public void dibujo_tecnico();
-}
--- proyect1/src/main/java/com/example/abstraccion/Figura.java
+++
@@ -1,9 +0,0 @@
-package com.example.abstraccion;
-
-
-public interface Figura {
-
-    public double calculo_area();
-
-    
-}
